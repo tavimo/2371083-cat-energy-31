fb2b745515d01409162724f4a05c1f5a4b694db4
Tatiana <tavimo14@gmail.com>
2024-05-31 21:20:29 +0500

    Правки JS

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,8 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-header');
+const navMain = document.querySelector('.main-header__navigation');
 const navToggle = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('.main-header__navigation--nojs');
+navMain.classList.remove('main-header__navigation--nojs');
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-header__navigation--closed')) {
