a9c213a63f3c6733c2b62a4eecf0324673954988
Tatiana <tavimo14@gmail.com>
2024-06-05 17:22:25 +0500

    Вносит правки JS

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,13 +3,20 @@ const navMain = document.querySelector('.main-header__navigation');
 const navToggle = document.querySelector('.main-header__toggle');
 
 navMain.classList.remove('main-header__navigation--nojs');
+navToggle.classList.remove('main-header__toggle--nojs');
+
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-header__navigation--closed')) {
     navMain.classList.remove('main-header__navigation--closed');
     navMain.classList.add('main-header__navigation--opened');
+    (navToggle.classList.contains('main-header__toggle--closed'));
+    navToggle.classList.remove('main-header__toggle--closed');
+    navToggle.classList.add('main-header__toggle--opened');
   } else {
     navMain.classList.add('main-header__navigation--closed');
     navMain.classList.remove('main-header__navigation--opened');
+    navToggle.classList.add('main-header__toggle--closed');
+    navToggle.classList.remove('main-header__toggle--opened');
   }
 });
